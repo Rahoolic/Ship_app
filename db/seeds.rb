8e4184b69adc50b562a90428b38912a3bef2fa65
Raul Gavris <ra.gavris@gmail.com>
2021-11-23 12:36:40 +0000

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Destroying boats"
+Boat.destroy_all
+puts "Boats destroyed"
+
+puts "Destroying users"
+User.destroy_all
+puts "Users destroyed"
+
+##1.time do |user|
+##  User.create!(
+##   email: test@test.com
+##  )
+##end
+
+puts "Creating users"
+User.create!(
+  email: "raul@test.com",
+  password: "123456"
+)
+User.create!(
+  email: "denise@test.com",
+  password: "123456"
+)
+User.create!(
+  email: "felix@test.com",
+  password: "123456"
+)
+User.create!(
+  email: "isabelle@test.com",
+  password: "123456"
+)
+puts "Users created"
+
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+puts "Creating boats"
+users = User.all
+users.each do |user|
+  rand(5..10).times do
+    Boat.create!(
+      user: user,
+      title: Faker::Name.name,
+      description: Faker::Lorem.paragraph(sentence_count: 3)
+    )
+  end
+end
+puts "Boats created"
+
+## use this for bookings: status: %w[available unavailable].sample
